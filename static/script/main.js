f084b6442b70c135cf2bd08a2f3f29bd40a50301
Glenardd <tolentinojanglenard5@gmail.com>
2023-04-02 01:54:52 +0800

    added humbergur button in navigation

--- static/script/main.js
+++ static/script/main.js
@@ -1,2 +1,22 @@
+let panel = document.getElementById('panel');
 
-  
+document.getElementById('burger-btn').addEventListener('click', hide)
+
+if (sessionStorage.getItem('save-panel') === "true") {
+    panel.style.display = 'none';    
+} 
+else if(sessionStorage.getItem('save-panel') === "false"){
+    panel.style.display = 'block';
+}
+
+//onclick event
+function hide(){
+    if(panel.style.display == 'none'){
+        panel.style.display = 'block';
+        sessionStorage.setItem('save-panel', "false");
+    }else{
+        console.log('hidden')
+        panel.style.display = 'none';    
+        sessionStorage.setItem('save-panel', "true");
+    }
+}
